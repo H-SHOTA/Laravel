3d587b96c1bd76e77baa341fe0cec554d949be9c
H-SHOTA <hukki.bou1213@gmail.com>
2015-09-02 00:24:43 +0900

    Laravel 4.2.0 ルーティング（ルートフィルターの冒頭までなんとか理解） http://readouble.com/laravel/4/2/0/ja/routing.html

--- app/routes.php
+++ app/routes.php
@@ -10,18 +10,63 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-
+// 基本のGETルート
 Route::get('/', function()
 {
-	return View::make('hello');
+	//$url = URL::to('foo');
+	// display 'http://localhost:8000/foo'
+	//var_dump($url);
+    //return 'Hello Root!';
+    return URL::route(‘profile’);;
+});
+// 基本のPOSTルート
+Route::post('foo/bar', function()
+{
+    return 'Hello foo/bar';
+});
+// 複数のHTTP動詞に対応するルートの登録
+Route::match(array('GET', 'POST'), '/match', function()
+{
+    return 'Hello match';
 });
 
-Route::get('users', function()
+// ルートパラメーター
+Route::get('id/{id}', function($id)
 {
-    $users = User::all();
+    return 'id '.$id;
+});
 
-//echo "<pre>";
-//var_dump($users);
-//echo "</pre>";
-    return View::make('users')->with('users', $users);
+// オプションのルートパラメーター
+Route::get('user/{name?}', function($name = 'ほげえええええええええええ！！')
+{
+    return $name;
+});
+
+// 正規表現によるルートの束縛
+Route::get('regex/{name}', function($name)
+{
+    //
+    return 'name:' . $name;
+})
+->where('name', '[A-Za-z]+');
+
+Route::get('regex/{id}', function($id)
+{
+    //
+    return 'id:' . $id;
+})
+->where('id', '[0-9]+');
+
+// ルートフィルター
+Route::get('nenrei/{age?}', array('before' => 'old', function($age = 0)
+{
+    return 'You are over 200 years old!';
+}));
+
+Route::filter('old', function()
+{
+    if (Route::input('age') < 200)
+    {
+    	return 'You are not over 200 years old!';
+    }
 });
